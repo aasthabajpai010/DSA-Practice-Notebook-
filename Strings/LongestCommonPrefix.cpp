e53069496bf9733ab536d819045c187104b4434a
AASTHA BAJPAI <2024aspire68@gmail.com>
2025-07-03 22:38:25 +0530

    Added Longest Common Prefix (Strings)
    Solved LeetCode 14: Longest Common Prefix using horizontal scanning.

---
+++ Strings/LongestCommonPrefix.cpp
@@ -0,0 +1,20 @@
+/*Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".*/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty())
+      return "";
+
+    for (int i = 0; i < strs[0].length(); ++i)
+      for (int j = 1; j < strs.size(); ++j)
+        if (i == strs[j].length() || strs[j][i] != strs[0][i])
+          return strs[0].substr(0, i);
+
+    return strs[0];
+  }
+};
+
+        
+    
+
